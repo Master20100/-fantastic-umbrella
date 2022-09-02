02bda33a81a8b0baa0cb9c588ba05e5e85ab6931
Mina Ghaly <minanabil@msn.com>
2022-09-02 23:10:02 +1000

    category get route fixed

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   const category = await Category.findAll({
     where: {
-      id: req.params
+      id: req.params.id
     }
     ,
     // be sure to include its associated Products
@@ -30,8 +30,13 @@ router.get('/:id', async(req, res) => {
   res.send(category);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  console.log(req.body)
+  await Category.create({
+    category_name:req.body.category
+  })
+  res.send(req.body);
 });
 
 router.put('/:id', (req, res) => {
